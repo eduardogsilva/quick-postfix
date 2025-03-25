e2d2d7ff3a5b0b8bd7aa90c11005065af529df31
Eduardo Silva <eduardo@eth0.com.br>
2025-03-25 18:18:52 -0300

    image build

---
+++ build_and_push.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+IMAGES=(
+  "eduardosilva/quick-postfix:latest"
+)
+
+build_images() {
+  echo "=========================================================================================="
+  echo "========== Starting the build of the images..."
+  docker compose -f docker-compose-build.yml build
+  if [ $? -eq 0 ]; then
+    echo "Build completed successfully."
+  else
+    echo "Error during the image build."
+    exit 1
+  fi
+}
+
+push_images() {
+  echo "=========================================================================================="
+  echo "========== Pushing images"
+  for IMAGE in "${IMAGES[@]}"; do
+    echo ""
+    echo ""
+    echo "=========================================================================================="
+    echo "========== Pushing image: $IMAGE..."
+    docker push "$IMAGE"
+    if [ $? -eq 0 ]; then
+      echo ""
+      echo "=== $IMAGE pushed successfully."
+      echo ""
+    else
+      echo ""
+      echo "=== ERROR PUSHING THE IMAGE: $IMAGE"
+      echo ""
+      exit 1
+    fi
+  done
+}
+
+docker system prune -a
+build_images
+push_images
+
+echo "=========================================================================================="
+echo "========== Build and push operations completed successfully."
